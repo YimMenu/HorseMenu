893a3e7d41f8d7671df6fe403c7ae5f75abca005
maybegreat48 <email@hostname>
2023-08-27 09:18:29 -0400

    fix: use upstream RDR-Classes

--- cmake/rdr-classes.cmake
+++ cmake/rdr-classes.cmake
@@ -2,8 +2,8 @@ include(FetchContent)
 
 FetchContent_Declare(
     rdr_classes
-    GIT_REPOSITORY https://github.com/maybegreat48/RDR-Classes.git
-    GIT_TAG        64aacd51bb65a74e2bd9ab0e6e6b39a84dc09525
+    GIT_REPOSITORY https://github.com/YimMenu/RDR-Classes.git
+    GIT_TAG        6bf009fae77419a9a1e249ddb711987422cf9822
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
