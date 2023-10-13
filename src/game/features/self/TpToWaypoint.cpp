afe31b90b20e21af61d7f0fa121cc6ab5271b8b1
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2023-10-13 18:50:16 +0200

    Readded teleport util files

--- src/game/features/self/TpToWaypoint.cpp
+++ src/game/features/self/TpToWaypoint.cpp
@@ -2,76 +2,10 @@
 #include "game/features/Features.hpp"
 #include "game/rdr/Natives.hpp"
 #include "game/backend/ScriptMgr.hpp"
+#include "util/teleport.hpp"
 
 namespace YimMenu::Features
 {
-	static bool LoadGroundAtCoords(Vector3& coords)
-	{
-		float groundZ;
-		bool done = false;
-
-		for (int i = 0; i < 20; i++)
-		{
-			for (int z = 0; z < 1000; z += 25)
-			{
-				float ground_iteration = static_cast<float>(z);
-				if (i >= 1 && (z % 100 == 0))
-				{
-					STREAMING::REQUEST_COLLISION_AT_COORD(coords.x, coords.y, ground_iteration);
-					ScriptMgr::Yield(1ms);
-				}
-				if (MISC::GET_GROUND_Z_FOR_3D_COORD(coords.x, coords.y, ground_iteration, &groundZ, false))
-				{
-					coords.z = groundZ + 1.f;
-					done     = true;
-				}
-			}
-
-			float height;
-			if (done && WATER::GET_WATER_HEIGHT(coords.x, coords.y, coords.z, &height))
-			{
-				coords.z = height + 1.f;
-			}
-
-			if (done)
-			{
-				return true;
-			}
-		}
-
-		//TODO notify
-		coords.z = 1000.f;
-
-		return false;
-	}
-
-	static bool TeleportEntity(Entity ent, Vector3& coords)
-	{
-		if (ENTITY::IS_ENTITY_A_PED(ent))
-		{
-			if (PED::IS_PED_ON_MOUNT(ent))
-				ent = PED::GET_MOUNT(ent);
-			if (PED::IS_PED_IN_ANY_VEHICLE(ent, false))
-				ent = PED::GET_VEHICLE_PED_IS_USING(ent);
-		}
-
-		//TOOD request control of entity
-		if (LoadGroundAtCoords(coords))
-		{
-			ENTITY::SET_ENTITY_COORDS_NO_OFFSET(ent, coords.x, coords.y, coords.z, false, false, false);
-		}
-
-		return true;
-	}
-
-	static Vector3 GetWaypointCoords()
-	{
-		if (MAP::IS_WAYPOINT_ACTIVE())
-			return MAP::_GET_WAYPOINT_COORDS();
-
-		//TODO notify
-		return Vector3{0, 0, 0};
-	}
 
 	class TpToWaypoint : public Command
 	{
@@ -81,8 +15,8 @@ namespace YimMenu::Features
 		{
 			if (MAP::IS_WAYPOINT_ACTIVE())
 			{
-				auto waypointCoords = GetWaypointCoords();
-				TeleportEntity(Self::PlayerPed, waypointCoords);
+				auto waypointCoords = Teleport::GetWaypointCoords();
+				Teleport::TeleportEntity(Self::PlayerPed, waypointCoords);
 			}
 		}
 	};
