2d3b13c9edcf832c36e735c276f2f2921350afcf
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2023-10-14 19:23:06 +0200

    Get personal horse as Self::Mount
    If last horse doesn't exist

--- src/game/features/Features.cpp
+++ src/game/features/Features.cpp
@@ -26,6 +26,8 @@ namespace YimMenu
 			Self::Mount = PED::GET_MOUNT(Self::PlayerPed);
 		else if (ENTITY::DOES_ENTITY_EXIST(PED::_GET_LAST_MOUNT(Self::PlayerPed)))
 			Self::Mount = PED::_GET_LAST_MOUNT(Self::PlayerPed);
+		else if(ENTITY::DOES_ENTITY_EXIST(PLAYER::GET_MOUNT_OWNED_BY_PLAYER(Self::Id)))
+			Self::Mount = PLAYER::GET_MOUNT_OWNED_BY_PLAYER(Self::Id);
 		else
 			Self::Mount = 0;
 	}
