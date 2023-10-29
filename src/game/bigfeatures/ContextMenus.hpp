c0e2bc3c711783fbcfc4bbe58706b892572219a1
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2023-10-29 09:47:40 +0100

    Update ContextMenus.hpp

--- src/game/bigfeatures/ContextMenus.hpp
+++ src/game/bigfeatures/ContextMenus.hpp
@@ -1,9 +1,12 @@
 #pragma once
 #include "ContextMenu.hpp"
+#include "core/commands/Commands.hpp"
 #include "game/backend/Players.hpp"
+#include "game/commands/PlayerCommand.hpp"
 #include "game/features/Features.hpp"
 #include "game/rdr/Natives.hpp"
 #include "util/teleport.hpp"
+
 
 namespace YimMenu
 {
@@ -23,16 +26,45 @@ namespace YimMenu
 					            break;
 				            }
 	            }},
-	        {"Teleport to player",
+	        {"Teleport to",
 	            [&] {
 		            auto playerPos = ENTITY::GET_ENTITY_COORDS(ContextMenu::GetEntityHandle(), false, true);
 		            Teleport::TeleportEntity(Self::PlayerPed, playerPos, false);
+	            }},
+	        {"Teleport Behind",
+	            [&] {
+		            auto playerCoords = ENTITY::GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(ContextMenu::GetEntityHandle(), 0, -10, 0);
+		            if (Teleport::TeleportEntity(Self::PlayerPed, playerCoords, true))
+			            g_Spectating = false;
 	            }},
 	        {"Explode",
 	            [&] {
 		            auto playerPos = ENTITY::GET_ENTITY_COORDS(ContextMenu::GetEntityHandle(), false, true);
 		            FIRE::ADD_EXPLOSION(playerPos.x, playerPos.y, playerPos.z, 22, 1.0f, true, false, 1.0f);
 	            }},
-
+	        {"Set Defensive",
+	            [&] {
+		            int playerId = -1;
+		            for (auto [id, plyr] : YimMenu::Players::GetPlayers())
+			            if (plyr.IsValid() && plyr.GetPed().GetPointer<void*>())
+				            if (ContextMenu::GetEntityHandle() == PLAYER::GET_PLAYER_PED_SCRIPT_INDEX(id))
+				            {
+					            playerId = id;
+					            break;
+				            }
+		            Commands::GetCommand<PlayerCommand>("defensive"_J)->Call(playerId);
+	            }},
+	        {"Set Offensive",
+	            [&] {
+		            int playerId = -1;
+		            for (auto [id, plyr] : YimMenu::Players::GetPlayers())
+			            if (plyr.IsValid() && plyr.GetPed().GetPointer<void*>())
+				            if (ContextMenu::GetEntityHandle() == PLAYER::GET_PLAYER_PED_SCRIPT_INDEX(id))
+				            {
+					            playerId = id;
+					            break;
+				            }
+		            Commands::GetCommand<PlayerCommand>("offensive"_J)->Call(playerId);
+	            }},
 	    });
 }
