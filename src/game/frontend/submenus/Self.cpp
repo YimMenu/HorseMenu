6d5ec0f05655d0680a31ba41cc9722184a0906c4
DayibBaba <79384354+DayibBaba@users.noreply.github.com>
2023-10-29 16:32:45 +0100

    Update Self.cpp

--- src/game/frontend/submenus/Self.cpp
+++ src/game/frontend/submenus/Self.cpp
@@ -1,9 +1,46 @@
 #include "Self.hpp"
 
+#include "game/backend/FiberPool.hpp"
+#include "game/backend/ScriptMgr.hpp"
+#include "game/features/Features.hpp"
 #include "game/frontend/items/Items.hpp"
+#include "game/rdr/Natives.hpp"
 
 namespace YimMenu::Submenus
 {
+	void RenderAnimationsCategory()
+	{
+		static std::string anim, dict;
+		InputTextWithHint("Dictionary", "Enter Dictionary Name", &dict).Draw();
+		InputTextWithHint("Animation", "Enter Animation Name", &anim).Draw();
+
+		if (ImGui::Button("Play Animation"))
+		{
+			FiberPool::Push([=] {
+				for (int i = 0; i < 250; i++)
+				{
+					if (dict.empty() || anim.empty())
+						break;
+
+					if (STREAMING::HAS_ANIM_DICT_LOADED(dict.c_str()))
+						break;
+
+					STREAMING::REQUEST_ANIM_DICT(dict.c_str());
+					ScriptMgr::Yield();
+				}
+
+				TASK::TASK_PLAY_ANIM(YimMenu::Self::PlayerPed, dict.c_str(), anim.c_str(), 8.0f, -8.0f, -1, 0, 0, false, false, false, "", 0);
+			});
+		}
+		ImGui::SameLine();
+		if (ImGui::Button("Stop"))
+		{
+			FiberPool::Push([=] {
+				TASK::CLEAR_PED_TASKS(YimMenu::Self::PlayerPed, true, false);
+			});
+		}
+	}
+
 	Self::Self() :
 	    Submenu::Submenu("Self")
 	{
@@ -14,8 +51,9 @@ namespace YimMenu::Submenus
 		auto columns       = std::make_shared<Column>(2);
 
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("godmode"_J));
-		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("noragdoll"_J));
+		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("invis"_J));
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("offtheradar"_J));
+		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("noragdoll"_J));
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("invis"_J));
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("antiafk"_J));
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("keepbarsfilled"_J));
@@ -29,6 +67,12 @@ namespace YimMenu::Submenus
 
 		toolsGroup->AddItem(std::make_shared<CommandItem>("suicide"_J));
 		toolsGroup->AddItem(std::make_shared<CommandItem>("clearcrimes"_J));
+		toolsGroup->AddItem(std::make_shared<ImGuiItem>([] {
+			if (ImGui::Button("Unfreeze"))
+				FiberPool::Push([] {
+					ENTITY::FREEZE_ENTITY_POSITION(YimMenu::Self::PlayerPed, false);
+				});
+		}));
 
 		movementGroup->AddItem(std::make_shared<BoolCommandItem>("noclip"_J));
 
@@ -40,22 +84,24 @@ namespace YimMenu::Submenus
 		AddCategory(std::move(main));
 
 		auto horse             = std::make_shared<Category>("Horse");
-		auto columnsHorse      = std::make_shared<Column>(2);
+		auto horseColumns             = std::make_shared<Column>(2);
 		auto horseGlobalsGroup = std::make_shared<Group>("Globals", GetListBoxDimensions());
-		auto horseToolsGroup   = std::make_shared<Group>("Tools", GetListBoxDimensions());
-
 		horseGlobalsGroup->AddItem(std::make_shared<BoolCommandItem>("horsegodmode"_J));
 		horseGlobalsGroup->AddItem(std::make_shared<BoolCommandItem>("horsenoragdoll"_J));
 		horseGlobalsGroup->AddItem(std::make_shared<BoolCommandItem>("keephorsebarsfilled"_J));
 		horseGlobalsGroup->AddItem(std::make_shared<BoolCommandItem>("keephorsecoresfilled"_J));
 		horseGlobalsGroup->AddItem(std::make_shared<BoolCommandItem>("keephorseagitationlow"_J));
-		horseToolsGroup->AddItem(std::make_shared<CommandItem>("tpmounttoself"_J));
-
-		columnsHorse->AddItem(horseGlobalsGroup);
-		columnsHorse->AddNextColumn();
-		columnsHorse->AddItem(horseToolsGroup);
-
-		horse->AddItem(columnsHorse);
+		horseGlobalsGroup->AddItem(std::make_shared<CommandItem>("tpmounttoself"_J));
+		horseColumns->AddItem(horseGlobalsGroup);
+		horse->AddItem(horseColumns);
 		AddCategory(std::move(horse));
+
+		auto animations = std::make_shared<Category>("Animations");
+
+		animations->AddItem(std::make_shared<ImGuiItem>([] {
+			RenderAnimationsCategory();
+		}));
+
+		AddCategory(std::move(animations));
 	}
 }
