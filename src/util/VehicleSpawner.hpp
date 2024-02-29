6fb48d41090019dc45468c2b5da79d665bcfdb22
Rxann <80224521+Rxann@users.noreply.github.com>
2024-02-29 16:23:43 -0500

    main

---
+++ src/util/VehicleSpawner.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "game/backend/ScriptMgr.hpp"
+#include "game/features/Features.hpp"
+#include "game/rdr/Natives.hpp"
+
+#include <cmath>
+namespace YimMenu
+{
+	inline bool SpawnVehicle(std::string model_name, Ped player_ped_id);
+}
