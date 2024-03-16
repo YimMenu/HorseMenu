3b441eb327a6ebf901c145362108403c04e7ca75
theofficialhackclean <85363951+theofficialhackclean@users.noreply.github.com>
2024-03-15 20:30:14 -0400

    Update weather.hpp

--- src/game/frontend/submenus/World/weather.hpp
+++ src/game/frontend/submenus/World/weather.hpp
@@ -24,11 +24,11 @@ inline const char* WeatherTypes[]{
     "WHITEOUT",
 };
 
-void RDR2_ChangeWeather(const char* weather)
+void ChangeWeather(const char* weather)
 {
 	MISC::_SET_OVERRIDE_WEATHER(MISC::GET_HASH_KEY(weather));
 }
-void RDR2_ChangeTime(int H = 12, int M = 0, int S = 0, int transition = 0, bool freeze = false)
+void ChangeTime(int H = 12, int M = 0, int S = 0, int transition = 0, bool freeze = false)
 {
 	NETWORK::_NETWORK_CLOCK_TIME_OVERRIDE(H, M, S, transition, freeze);
 }
