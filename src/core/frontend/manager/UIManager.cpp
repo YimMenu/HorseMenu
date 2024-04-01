65a340199fe8752cd35023f783f1e413cfb72807
DeadlineEm <jownsall@gmail.com>
2024-04-01 14:30:04 -0400

    Update UIManager.cpp
    Added Game Build Version at bottom left.

--- src/core/frontend/manager/UIManager.cpp
+++ src/core/frontend/manager/UIManager.cpp
@@ -31,7 +31,12 @@ namespace YimMenu
 		}
 		ImGui::EndChild();
 		
-		ImGui::Text("HorseMenu");
+		std::string gameVer = DEBUG::GET_GAME_VERSION_NAME();
+		size_t spos = gameVer.find("_dev_live_tu");
+		if (spos != std::string::npos) {
+			gameVer.erase(spos); // removes _dev_live_tu from the game build version
+		}
+		ImGui::Text("Build Ver. %s", gameVer.c_str()); // Outputs the current game build version number.
 
 		pos.y -= 28;
 		ImGui::SetCursorPos(ImVec2(pos.x + 130, pos.y));
