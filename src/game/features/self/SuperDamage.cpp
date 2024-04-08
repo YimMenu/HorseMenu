d68767368de87c59900a05620f79407f78d3c533
DeadlineEm <jownsall@gmail.com>
2024-04-08 16:08:04 -0400

    Update SuperDamage.cpp
    Removed Melee as it is now Super Punch in (PR #130)

--- src/game/features/self/SuperDamage.cpp
+++ src/game/features/self/SuperDamage.cpp
@@ -11,17 +11,17 @@ namespace YimMenu::Features
 
 		virtual void OnTick() override
 		{
-				PLAYER::SET_PLAYER_WEAPON_DAMAGE_MODIFIER(Self::PlayerPed, std::numeric_limits<float>::max());
-				PLAYER::SET_PLAYER_MELEE_WEAPON_DAMAGE_MODIFIER(Self::PlayerPed, std::numeric_limits<float>::max());
-				PLAYER::_SET_PLAYER_EXPLOSIVE_WEAPON_DAMAGE_MODIFIER(Self::PlayerPed, std::numeric_limits<float>::max());
+			PLAYER::SET_PLAYER_WEAPON_DAMAGE_MODIFIER(Self::PlayerPed, 1000000.0);
+			PLAYER::_SET_PLAYER_EXPLOSIVE_WEAPON_DAMAGE_MODIFIER(Self::PlayerPed, 1000000.0);
+			NETWORK::_SET_LOCAL_PLAYER_DAMAGE_MULTIPLIER_FOR_PLAYER(Self::PlayerPed, 1000000.0)
 		}
-
+		
 		virtual void OnDisable() override
 		{
 			// Set all player weapon damage modifiers to their defaults
 			PLAYER::SET_PLAYER_WEAPON_DAMAGE_MODIFIER(Self::PlayerPed, 1.0);
-			PLAYER::SET_PLAYER_MELEE_WEAPON_DAMAGE_MODIFIER(Self::PlayerPed, 1.0);
 			PLAYER::_SET_PLAYER_EXPLOSIVE_WEAPON_DAMAGE_MODIFIER(Self::PlayerPed, 1.0);
+			NETWORK::_SET_LOCAL_PLAYER_DAMAGE_MULTIPLIER_FOR_PLAYER(Self::PlayerPed, 1.0)
 		}
 	};
 
