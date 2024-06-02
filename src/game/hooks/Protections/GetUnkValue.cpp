609200295bfb8ec907c178813ad9549674f75d7b
Rxann <80224521+Rxann@users.noreply.github.com>
2024-06-02 18:26:11 -0400

    Don't show notifications due to false positives

--- src/game/hooks/Protections/GetUnkValue.cpp
+++ src/game/hooks/Protections/GetUnkValue.cpp
@@ -1,16 +1,14 @@
-#include "core/frontend/Notifications.hpp"
 #include "core/hooking/DetourHook.hpp"
 #include "game/hooks/Hooks.hpp"
 
 
 namespace YimMenu::Hooks
 {
-	// there seem to be some false positives with this
+	// there seem to be some false positives with this - just incase we can return 0
 	int Protections::GetUnkValue(int64_t* a1, unsigned int* a2)
 	{
 		if (auto ptr = *(uint64_t*)(a1 + 0xC10); !ptr)
 		{
-			Notifications::Show("Protections", "Blocked Fortitude's Nemesis Streaming Crash", NotificationType::Warning);
 			return 0;
 		}
 
