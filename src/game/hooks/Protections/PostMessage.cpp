41304137e46acb7b021703114c47efa5d221fe3a
Rxann <80224521+Rxann@users.noreply.github.com>
2024-06-16 15:55:02 -0400

    Log PostMessage

--- src/game/hooks/Protections/PostMessage.cpp
+++ src/game/hooks/Protections/PostMessage.cpp
@@ -1,20 +1,30 @@
+#include "core/commands/BoolCommand.hpp"
 #include "core/hooking/DetourHook.hpp"
 #include "game/hooks/Hooks.hpp"
 
 #include <network/rlGamerInfo.hpp>
+
+namespace YimMenu::Features
+{
+	BoolCommand _LogPostMessage{"logpostmessage", "Log Post Message", "Log messages sent via Post Message"};
+}
 
 
 namespace YimMenu::Hooks
 {
 	bool Protections::PPostMessage(int localGamerIndex, rage::rlGamerInfo* recipients, int numRecipients, const char* msg, unsigned int ttlSeconds)
 	{
-		LOG(VERBOSE) << "Local Gamer Index: " << localGamerIndex;
-		LOG(VERBOSE) << "Recipients name and rid: " << std::string(recipients->m_Name).c_str() << " "
-		             << recipients->m_GamerHandle.m_rockstar_id;
+		if (Features::_LogPostMessage.GetState())
+		{
+			LOG(VERBOSE) << "Local Gamer Index: " << localGamerIndex;
+			LOG(VERBOSE) << "Recipients name and rid: " << std::string(recipients->m_Name).c_str() << " "
+			             << recipients->m_GamerHandle.m_rockstar_id;
 
-		LOG(VERBOSE) << "Num recipients: " << numRecipients;
-		LOG(VERBOSE) << "Message: " << msg;
-		LOG(VERBOSE) << "TTL: " << ttlSeconds;
+			LOG(VERBOSE) << "Num recipients: " << numRecipients;
+			LOG(VERBOSE) << "Message: " << msg;
+			LOG(VERBOSE) << "TTL: " << ttlSeconds;
+		}
+
 
 		return BaseHook::Get<Protections::PPostMessage, DetourHook<decltype(&Protections::PPostMessage)>>()->Original()(localGamerIndex, recipients, numRecipients, msg, ttlSeconds);
 	}
