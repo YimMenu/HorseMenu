89fdee5a81b5c2a06f203456f9ed1045f20192b3
Rxann <80224521+Rxann@users.noreply.github.com>
2024-07-03 18:54:59 -0400

    Bump RDR-Classes

--- cmake/rdr-classes.cmake
+++ cmake/rdr-classes.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     rdr_classes
     GIT_REPOSITORY https://github.com/YimMenu/RDR-Classes.git
-    GIT_TAG        722e3276338609dc980b15d2aeb38263300318fd
+    GIT_TAG        f4c328d3c9c3e67d8dda8e36d1e2658ad35dcb0e
     GIT_PROGRESS TRUE
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
