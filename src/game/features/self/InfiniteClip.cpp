1fb1a24a0e30b6eecdeb25d73fd98a0ad6cc5b1d
Rxann <80224521+Rxann@users.noreply.github.com>
2024-07-09 18:23:56 -0400

    Fix InfiniteClip

--- src/game/features/self/InfiniteClip.cpp
+++ src/game/features/self/InfiniteClip.cpp
@@ -5,22 +5,18 @@
 
 namespace YimMenu::Features
 {
-	//WEAPON::_SET_PED_INFINITE_AMMO_CLIP Changes too many things to be considered, such as granting all ammo types.
 	class InfiniteClip : public LoopedCommand
 	{
 		using LoopedCommand::LoopedCommand;
 
 		virtual void OnTick() override
 		{
-			Hash current_weapon = WEAPON::_GET_PED_CURRENT_HELD_WEAPON(Self::PlayerPed);
-			int current_clip_ammo{};
-			
-			WEAPON::GET_AMMO_IN_CLIP(Self::PlayerPed, &current_clip_ammo, current_weapon);
-			
-			auto clip_size = WEAPON::GET_WEAPON_CLIP_SIZE(current_weapon);
+			WEAPON::_SET_PED_INFINITE_AMMO_CLIP(Self::PlayerPed, TRUE);
+		}
 
-			if (current_clip_ammo < clip_size)
-				WEAPON::_REFILL_AMMO_IN_CURRENT_PED_WEAPON(Self::PlayerPed);
+		virtual void OnDisable() override
+		{
+			WEAPON::_SET_PED_INFINITE_AMMO_CLIP(Self::PlayerPed, FALSE);
 		}
 	};
 
