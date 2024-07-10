813bcdae1aea1c191bb64b316807ddd4167148d8
DeadlineEm <jownsall@gmail.com>
2024-07-09 20:45:07 -0400

    Update EagleEye.cpp

--- src/game/features/self/EagleEye.cpp
+++ src/game/features/self/EagleEye.cpp
@@ -15,8 +15,7 @@ namespace YimMenu::Features
 			if (!PLAYER::_IS_SECONDARY_SPECIAL_ABILITY_ACTIVE(YimMenu::Self::Id)) // Checks if Eagle Eye is active before toggling it on
 			{
 				PLAYER::_SECONDARY_SPECIAL_ABILITY_SET_ACTIVE(YimMenu::Self::Id); // Toggles Eagle Eye on
-				PLAYER::_EAGLE_EYE_SET_PLUS_FLAG_DISABLED(YimMenu::Self::Id, false); // Allows running while eagle eye is active
-				Notifications::Show("Eagle Eye", "Infinite Eagle eye has been enabled.  This allows you to run while eagle eye is active as well.", NotificationType::Success);
+				PLAYER::_EAGLE_EYE_SET_PLUS_FLAG_DISABLED(YimMenu::Self::Id, false); // Allows running while eagle eye is active	
 			}
 			PLAYER::_MODIFY_INFINITE_TRAIL_VISION(YimMenu::Self::Id, 1); // Sets Eagle Eye to Infinite
 		}
@@ -26,7 +25,6 @@ namespace YimMenu::Features
 			PLAYER::_SECONDARY_SPECIAL_ABILITY_SET_DISABLED(YimMenu::Self::Id, 1); // Disables Eagle Eye
 			PLAYER::_MODIFY_INFINITE_TRAIL_VISION(YimMenu::Self::Id, 0);           // Turns off Infinite Eagle Eye
 			PLAYER::_EAGLE_EYE_SET_PLUS_FLAG_DISABLED(YimMenu::Self::Id, NULL); //Setting to truee breaks it, leave it at NULL
-			Notifications::Show("Eagle Eye", "Infinite Eagle eye has been disabled.", NotificationType::Success);
 		}
 	};
 
