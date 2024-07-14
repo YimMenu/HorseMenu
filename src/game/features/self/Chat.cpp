aaa55295a68e3dc2042d46e85f92848dd30fb167
Rxann <80224521+Rxann@users.noreply.github.com>
2024-07-13 23:02:42 -0400

    Change casing of variable names

--- src/game/features/self/Chat.cpp
+++ src/game/features/self/Chat.cpp
@@ -21,20 +21,20 @@ namespace YimMenu::Features
 			if (*Pointers.IsSessionStarted && !SCRIPTS::IS_LOADING_SCREEN_VISIBLE() && MISC::UPDATE_ONSCREEN_KEYBOARD() != 0 && !GUI::IsOpen())
 			{
 				ScriptMgr::Yield(100ms); // Delay so hotkey key doesn't get mistaken as input
-				bool is_chat_cancelled = false;
+				bool isChatCancelled = false;
 				MISC::DISPLAY_ONSCREEN_KEYBOARD(0, "Chat Message", "", "", "", "", "", 256);
 				while (MISC::UPDATE_ONSCREEN_KEYBOARD() == 0)
 				{
 					if (GetAsyncKeyState(VK_ESCAPE) & 0x8000)
 					{
 						MISC::CANCEL_ONSCREEN_KEYBOARD();
-						is_chat_cancelled = true;
+						isChatCancelled = true;
 					}
 
 					ScriptMgr::Yield();
 				}
 
-				if (!is_chat_cancelled)
+				if (!isChatCancelled)
 					SendChatMessage(MISC::GET_ONSCREEN_KEYBOARD_RESULT());
 			}
 			else
